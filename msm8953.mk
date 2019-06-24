9a282d5583ef3398852eea4650e257a1f5eefe55
Garret Kelly <gdk@google.com>
2019-06-24 11:54:02 -0400

    msm8953-common: Set BOOT_SECURITY_PATCH during build
    This property is now used during boot to provide the TEE-backed
    keymaster with the correct boot SPL.  Set it to the platform SPL until
    it diverges as part of an update.
    
    Bug: 119549677
    Test: verified boot.img has property using avbtool
    Change-Id: I6d96404918cf18d0fee79c701394b39baf349c53
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- msm8953.mk
+++ msm8953.mk
@@ -16,6 +16,9 @@
 
 $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 
+# Set boot SPL
+BOOT_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
