737c85c720efa1e4d5dcfdd6e03a6aa34c8cc9f4
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 18:34:18 +0530

    msm8953-common: Build Broken Phony

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -38,6 +38,9 @@ TARGET_USES_64_BIT_BINDER := true
 # APEX image
 DEXPREOPT_GENERATE_APEX_IMAGE := true
 
+# Broken PHONY
+BUILD_BROKEN_PHONY_TARGETS := true
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true
