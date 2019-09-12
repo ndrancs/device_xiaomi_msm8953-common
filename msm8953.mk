bd71f0627f145cb87fd185de6ff0c07ad06fa1cc
RevanthTolety <crevanthtolety@gmail.com>
2019-09-12 21:16:35 +0530

    msm8953-common: Nuke PRODUCT_ENFORCE_RRO_TARGETS

--- msm8953.mk
+++ msm8953.mk
@@ -20,7 +20,7 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
+# PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
 # Screen density
