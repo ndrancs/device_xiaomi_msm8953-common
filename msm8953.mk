1f926651adced984db2cd1ac7fca9e9c51bbe35a
RevanthTolety <crevanthtolety@gmail.com>
2019-09-17 04:12:34 +0000

    msm8953-common: Enforce All RRO Targets

--- msm8953.mk
+++ msm8953.mk
@@ -20,8 +20,8 @@ $(call inherit-product, vendor/xiaomi/msm8953-common/msm8953-common-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-# PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+# RRO
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
